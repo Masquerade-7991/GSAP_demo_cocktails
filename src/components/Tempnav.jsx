a0088d6ebbef5403c4a8688da56cbae3370a9a02
Masquerade-7991 <127243063+Masquerade-7991@users.noreply.github.com>
2025-07-29 19:01:05 +0530

    Update Tempnav.jsx

--- src/components/Tempnav.jsx
+++ src/components/Tempnav.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       },
       {
         backgroundColor: "##00000050",
-        backdropFilter: "blur(20px)",
+        backdropFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
       }
