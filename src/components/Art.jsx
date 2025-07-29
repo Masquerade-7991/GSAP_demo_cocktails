099851a66ffb905faf1b1deca70f246e1e800cf7
Masquerade-7991 <127243063+Masquerade-7991@users.noreply.github.com>
2025-07-29 19:09:31 +0530

    Update Art.jsx

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -32,7 +32,7 @@ const Art = () => {
   });
 
   return (
-    <div id="art work">
+    <div id="art">
       <div className="container mx-auto h-full pt-20">
         <h2 className="will-fade">The ART</h2>
 
